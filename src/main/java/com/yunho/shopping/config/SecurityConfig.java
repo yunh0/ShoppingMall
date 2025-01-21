0877de3db84d2bf9b1204bdc0c8358da72dda003
yunho <nam100400@naver.com>
2025-01-21 15:38:10 +0900

    Add SecurityConfig

---
+++ src/main/java/com/yunho/shopping/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.yunho.shopping.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .build();
+    }
+}
