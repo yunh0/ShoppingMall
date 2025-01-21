7e572931a30c0b440ffec030b3a7d01b439c355d
yunho <nam100400@naver.com>
2025-01-21 15:22:57 +0900

    Add ThymeleafConfig

--- src/main/java/com/yunho/shopping/ShoppingApplication.java
+++ src/main/java/com/yunho/shopping/ShoppingApplication.java
@@ -2,8 +2,10 @@ package com.yunho.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ShoppingApplication {
 
     public static void main(String[] args) {
