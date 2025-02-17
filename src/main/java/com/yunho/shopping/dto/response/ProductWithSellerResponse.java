1cc4a483a52f406025fec571f5d7a931a02a80f4
yunho <nam100400@naver.com>
2025-02-17 17:24:26 +0900

    Update - 판매중인 상품 리스트에 카테고리 추가
    - 판매중인 상품 리스트에 카테고리 추가함
    - 카테고리 계층 구조로 표시됨
      ex) 남성의류 - 상의 - 셔츠
    - 카테고리로 정렬 가능

--- src/main/java/com/yunho/shopping/dto/response/ProductWithSellerResponse.java
+++ src/main/java/com/yunho/shopping/dto/response/ProductWithSellerResponse.java
@@ -1,14 +1,19 @@
 package com.yunho.shopping.dto.response;
 
+import com.yunho.shopping.domain.Category;
 import com.yunho.shopping.dto.ProductDto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public record ProductWithSellerResponse(
         Long productId,
         String productName,
         Integer price,
         Integer count,
+        Category category,
         LocalDateTime createdAt
 ) {
         public static ProductWithSellerResponse of(
@@ -16,9 +21,10 @@ public record ProductWithSellerResponse(
                 String productName,
                 Integer price,
                 Integer count,
+                Category category,
                 LocalDateTime createdAt
         ){
-            return new ProductWithSellerResponse(productId, productName, price, count, createdAt);
+            return new ProductWithSellerResponse(productId, productName, price, count, category, createdAt);
         }
 
         public static ProductWithSellerResponse from(ProductDto productDto){
@@ -27,7 +33,22 @@ public record ProductWithSellerResponse(
                     productDto.productName(),
                     productDto.price(),
                     productDto.count(),
+                    productDto.category(),
                     productDto.createdAt()
             );
+        }
+
+        public String categoryHierarchy() {
+            return getCategoryHierarchy(this.category);
+        }
+
+        private String getCategoryHierarchy(Category category) {
+            List<String> hierarchy = new ArrayList<>();
+            while (category != null) {
+                hierarchy.add(category.getName());
+                category = category.getParentCategory();
+            }
+            Collections.reverse(hierarchy);
+            return String.join(" - ", hierarchy);
         }
 }
